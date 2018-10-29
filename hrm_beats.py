6cc115fcaf3c333d6a007a1f397519c196ab2a1d
dengzengtian <dengcengtian@dengceniandembp.home>
2018-10-28 23:57:10 -0400

    updated docstring for one file

--- hrm_beats.py
+++ hrm_beats.py
@@ -1,4 +1,24 @@
 def hrm_beats(peak, metrics):
+    """
+
+    Parameters
+    ----------
+    peak : ndarry(dtype=float, ndim=2)
+        [[time, voltage]]
+        Array containing the peak voltage and related time
+    metrics : dict
+        "mean_hr_bpm": float
+        "voltage_extremes": tuple
+        "duration": float
+        "num_beats": int
+        "beats": list
+        dictionary containing the metrics of ECG data
+
+    Returns
+    -------
+    metrics: dict
+    same dictionary as in parameters with updated "beats" entry
+    """
     import numpy as np
     beats = np.array([i[0] for i in peak])
     metrics["beats"] = beats.tolist()
